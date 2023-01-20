b007b259a43e27bc534deee89dbab4f978736dd3
Vasamel <super.ferarry@gmail.com>
2023-01-20 05:59:18 +0200

    add file

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+  timerId = setInterval(() => {
+    const randomHexColor = getRandomHexColor();
+    bodyEl.setAttribute('style', `background-color: ${randomHexColor}`);
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+});
